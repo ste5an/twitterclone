66fa8d6acb9d00a4fa7ee72bc56eab4cb0f42bb2
papst <etienne13@me.com>
2020-05-25 17:48:47 +0300

    Basic CRUD operation added Database configuration

---
+++ src/main/java/com/ste5an/twitter/repository/MessageRepository.java
@@ -0,0 +1,11 @@
+package com.ste5an.twitter.repository;
+
+import com.ste5an.twitter.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+}
